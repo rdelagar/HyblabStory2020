da8e3a2a701e3c6293867cee95ff9c0e57eed3a2
Remi <remi.delagarde@orange.fr>
2020-01-25 11:08:31 +0100

    Test scroll avec navigateur Safari sur le serveur

--- bout-du-monde/public/js/map.js
+++ bout-du-monde/public/js/map.js
@@ -1,7 +1,24 @@
+/*let chrome   = navigator.userAgent.indexOf('Chrome') > -1;
+let explorer = navigator.userAgent.indexOf('MSIE') > -1;
+let firefox  = navigator.userAgent.indexOf('Firefox') > -1;
+let safari   = navigator.userAgent.indexOf("Safari") > -1;
+let camino   = navigator.userAgent.indexOf("Camino") > -1;
+let opera    = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+if ((chrome) && (safari)) safari = false;
+if ((chrome) && (opera)) chrome = false;
+
+let goodBrowsers;
+
+if(chrome || firefox || opera) {
+    goodBrowsers = true;
+} else {
+    goodBrowsers = false;
+}*/
+
 $(document).ready(function () {
 
     $("#sec-story").hide();
-    $("#start-story").hide();
+    $("#start-story").show();
 
     mapboxgl.accessToken = config.accessToken;
 
