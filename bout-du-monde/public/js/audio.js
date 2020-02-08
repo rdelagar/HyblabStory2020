305420badefe9f37e637192b804ee2b0f4f5127b
tod01 <git@bitbucket.org:tod01>
2020-02-08 19:35:59 +0100

    Sound management

--- bout-du-monde/public/js/audio.js
+++ bout-du-monde/public/js/audio.js
@@ -1,83 +1,142 @@
 /***
 JS by https://codepen.io/GRSimon/
 ***/
-var getaudio = $('#player')[0],
+
+
+$(".audio span").click(function() {
+   
+   var getaudio = $(this).parent()[0].nextElementSibling,
      mouseovertimer,
      audiostatus = 'off',
-     playerControls = ".player-controls";
-
-$(document).on('mouseenter', playerControls, function() {
+     playerControls = "#" + $(this).parent()[0].id;
+   //console.log(geta)
+   $(document).on('mouseenter', playerControls, function() {
    
-   if (!mouseovertimer) {
-      
-      mouseovertimer = window.setTimeout(function() {
+      if (!mouseovertimer) {
          
-         mouseovertimer = null;
-         
-         if (!$(playerControls).hasClass("playing")) {
+         mouseovertimer = window.setTimeout(function() {
             
-            getaudio.load();
-            getaudio.play();            
-            $(playerControls).addClass('playing');
-            return false;
+            mouseovertimer = null;
+           
+            if (!$(playerControls).hasClass("playing")) {
+               var isPlaying = getaudio.currentTime > 0 && !getaudio.paused && !getaudio.ended && getaudio.readyState > 2;
+
+               if (!isPlaying) {
+                  getaudio.load(); 
+                  var playPromise = getaudio.play();
+                  if (playPromise !== undefined) {
+                     playPromise.then(_ => {
+                        $(playerControls).addClass('playing');
+                        return false; 
+                     })
+                     .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                     });
+                  }
+                  
+               }
+            }
             
-         }
-         
-      }, 2500);
-      
-   }
-   
-});
-
-$(document).on('mouseleave', playerControls, function() {
-   
-   if (mouseovertimer) {
-      
-      window.clearTimeout(mouseovertimer);
-      mouseovertimer = null;
-      
-   }
-   
-});
-
-$(document).on('click touch', playerControls, function(e) {
-   
-   e.preventDefault();
-   
-   if (!$(playerControls).hasClass("playing")) {
-
-      if (audiostatus == 'off') {
-         
-         $(playerControls).addClass('playing');
-         getaudio.load();
-         getaudio.play();
-         window.clearTimeout(mouseovertimer);
-         audiostatus = 'on';
-         return false;
-         
-      } else if (audiostatus == 'on') {
-         
-         $(playerControls).addClass('playing');
-         getaudio.play();
+         }, 2500);
          
       }
       
-   } else if ($(playerControls).hasClass("playing")) {
+   });
+   
+   $(document).on('mouseleave', playerControls, function() {
       
+      if (mouseovertimer) {
+         
+         window.clearTimeout(mouseovertimer);
+         mouseovertimer = null;
+         
+      }
+      
+   });
+   
+   $(document).on('click touch', playerControls, function(e) {
+      
+      e.preventDefault();
+      
+      if (!$(playerControls).hasClass("playing")) {
+   
+         if (audiostatus == 'off') {
+            var isPlaying = getaudio.currentTime > 0 && !getaudio.paused && !getaudio.ended && getaudio.readyState > 2;
+
+               if (!isPlaying) {
+                  $(playerControls).addClass('playing');
+                  getaudio.load();
+                  var playPromise = getaudio.play();
+
+                  if (playPromise !== undefined) {
+                     playPromise.then(_ => {
+                        window.clearTimeout(mouseovertimer);
+                        audiostatus = 'on';
+                        return false;
+                     })
+                     .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                     });
+                  }
+                  
+               }
+         } else if (audiostatus == 'on') {
+            var isPlaying = getaudio.currentTime > 0 && !getaudio.paused && !getaudio.ended && getaudio.readyState > 2;
+            if (!isPlaying) {
+               $(playerControls).addClass('playing');
+               var playPromise = getaudio.play();
+
+                  if (playPromise !== undefined) {
+                     playPromise.then(_ => {
+                        
+                     })
+                     .catch(error => {
+                        // Auto-play was prevented
+                        // Show paused UI.
+                     });
+                  }
+            }
+         }
+         
+      } else if ($(playerControls).hasClass("playing")) {
+         
+         getaudio.pause();
+         $(playerControls).removeClass('playing');
+         window.clearTimeout(mouseovertimer);
+         audiostatus = 'on';
+         
+      }
+      
+      return false;
+      
+   });
+   
+   $(this).next().on('ended', function() {
+      
+      $(playerControls).removeClass('playing');
+      audiostatus = 'off';
+      
+   });
+
+   $(window).on("wheel", function (e) {
+      
+      if(scroll) {
+         console.log("la");
+         getaudio.pause();
+         $(playerControls).removeClass('playing');
+         window.clearTimeout(mouseovertimer);
+         audiostatus = 'off';
+      }
+  });
+
+  $("button.next").on("click", function () {
+      console.log("btn");
       getaudio.pause();
       $(playerControls).removeClass('playing');
       window.clearTimeout(mouseovertimer);
-      audiostatus = 'on';
-      
-   }
-   
-   return false;
-   
-});
+      audiostatus = 'off';
+  });
 
-$('#player').on('ended', function() {
-   
-   $(playerControls).removeClass('playing');
-   audiostatus = 'off';
-   
 });
